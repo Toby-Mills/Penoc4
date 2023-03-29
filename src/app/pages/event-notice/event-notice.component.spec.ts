b6652a8b884527d3afba840d4aa28ee289076f21
Toby Mills <toby_mills@trimble.com>
2023-03-29 09:50:20 +0200

    added new EventNotice component

---
+++ src/app/pages/event-notice/event-notice.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventNoticeComponent } from './event-notice.component';
+
+describe('EventNoticeComponent', () => {
+  let component: EventNoticeComponent;
+  let fixture: ComponentFixture<EventNoticeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EventNoticeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EventNoticeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/event-notice/event-notice.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Input } from '@angular/core';
+import { OEvent } from 'src/app/models/oevent.model';
+import { PenocApiService } from 'src/app/services/penoc-api.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-event-notice',
+  templateUrl: './event-notice.component.html',
+  styleUrls: ['./event-notice.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class EventNoticeComponent implements OnInit {
+  public eventId: number = 0;
+  public oevent: OEvent = new OEvent();
+
+  constructor(private api: PenocApiService, private route: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.loadEvent(Number(this.route.snapshot.paramMap.get('id')));
+  }
+
+  public loadEvent(oeventId: number) {
+    this.api.getOEvent(oeventId).subscribe(result => {this.oevent = result;})
+  }
+}
