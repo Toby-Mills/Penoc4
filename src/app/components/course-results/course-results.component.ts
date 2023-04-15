152f3f77c8d0448e8256a3c3672315f84ce90ac9
Toby Mills <toby_mills@trimble.com>
2023-04-15 10:30:25 +0200

    display result comment on new line on narrow scrn

--- src/app/components/course-results/course-results.component.ts
+++ src/app/components/course-results/course-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, Host } from '@angular/core';
 import { CourseResults } from 'src/app/models/course-results';
 
 @Component({
@@ -7,18 +7,28 @@ import { CourseResults } from 'src/app/models/course-results';
   styleUrls: ['./course-results.component.css']
 })
 export class CourseResultsComponent implements OnInit {
-@Input() courseResults:CourseResults = new CourseResults;
+  @Input() courseResults: CourseResults = new CourseResults;
+  public narrowScreen: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.checkScreenWidth();
   }
 
-  public hasDisqualifiedCompetitors():boolean{
-    if(this.courseResults){
-          return this.courseResults.results.some(d => d.disqualified);
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any){
+    this.checkScreenWidth();
+  }
+
+  public hasDisqualifiedCompetitors(): boolean {
+    if (this.courseResults) {
+      return this.courseResults.results.some(d => d.disqualified);
     }
     return false;
+  }
 
+  private checkScreenWidth(){
+    this.narrowScreen = (window.innerWidth < 800 ? true: false);
   }
 }
