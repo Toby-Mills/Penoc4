bb52e4d256c803f525e4e772fd41084222fe568f
Toby Mills <toby_mills@trimble.com>
2023-05-05 10:30:26 +0200

    fixed bug in sortOEventResultsByDate

--- src/app/pipes/sort-oevent-results-by-date.pipe.ts
+++ src/app/pipes/sort-oevent-results-by-date.pipe.ts
@@ -9,10 +9,12 @@ export class SortOEventResultsByDatePipe implements PipeTransform {
   transform(value: Array<OEventResults>): Array<OEventResults> {
     if (value && value.length > 0) {
       return value.sort(function (a, b) {
-        if (a.oEvent && b.oEvent) {
-          if (a.oEvent.date > b.oEvent.date) {
+        if (a.oEvent && a.oEvent.date && b.oEvent && b.oEvent.date) {
+          let dateA = new Date(a.oEvent.date);
+          let dateB = new Date(b.oEvent.date);
+          if (dateA > dateB) {
             return -1;
-          } else if(a.oEvent.date == b.oEvent.date){
+          } else if(dateA == dateB){
             return 0
           } else {
             return 1;
