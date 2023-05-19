1c6ce568f815f33cfd69849ada3050deee51d72a
Toby Mills <toby_mills@trimble.com>
2023-05-19 15:28:45 +0200

    Added basic UI to event-edit page

--- src/app/pages/admin/event-edit/event-edit.component.ts
+++ src/app/pages/admin/event-edit/event-edit.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Club } from 'src/app/models/club';
+import { OEvent } from 'src/app/models/oevent.model';
+import { Venue } from 'src/app/models/venue';
+import { DataCacheService } from 'src/app/services/data-cache.service';
+import { PenocApiService } from 'src/app/services/penoc-api.service';
 
 @Component({
   selector: 'app-event-edit',
   templateUrl: './event-edit.component.html',
   styleUrls: ['./event-edit.component.css']
 })
-export class EventEditComponent {
+export class EventEditComponent implements OnInit {
+  public oEvent: OEvent | undefined;
 
+  public venues: Venue[] = [];
+  public clubs: Club[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private api: PenocApiService,
+    private dataCache: DataCacheService,
+  ) { }
+
+  public ngOnInit() {
+    this.dataCache.getVenues().subscribe(
+      { next: data => this.venues = data }
+    )
+    this.dataCache.getClubs().subscribe(
+      { next: data => {this.clubs = data;console.log(data)},
+      error: data => console.log(data) }
+    )
+    this.loadEvent(Number(this.route.snapshot.paramMap.get('oEventId')));
+  }
+
+  private loadEvent(oEventId: number) {
+    this.api.getOEvent(oEventId).subscribe(data => {
+      this.oEvent = data;
+    });
+  }
+
+  public onSaveClick() {
+    if (this.oEvent) {
+      this.api.saveOEvent(this.oEvent).subscribe(data => this.oEvent = data);
+    }
+
+  }
 }
