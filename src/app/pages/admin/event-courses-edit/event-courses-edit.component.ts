191fc883d1f2bfc306904c70cef46aa9c87eaed1
Toby Mills <toby_mills@trimble.com>
2023-10-12 12:53:51 +0200

    Added Event Info to event-course-edit

--- src/app/pages/admin/event-courses-edit/event-courses-edit.component.ts
+++ src/app/pages/admin/event-courses-edit/event-courses-edit.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Course } from 'src/app/models/course';
 import { Difficulty } from 'src/app/models/difficulty';
+import { OEvent } from 'src/app/models/oevent.model';
 import { DataCacheService } from 'src/app/services/data-cache.service';
 import { PenocApiService } from 'src/app/services/penoc-api.service';
 
@@ -11,65 +12,74 @@ import { PenocApiService } from 'src/app/services/penoc-api.service';
   styleUrls: ['./event-courses-edit.component.css']
 })
 export class EventCoursesEditComponent {
-  public eventId:number = 0;
-  public courses:Course[] = [];
-  public editCourse:Course|undefined = undefined;
-  public difficulties:Difficulty[] = [];
+  //public eventId:number = 0;
+  public oEvent: OEvent | undefined = undefined;
+  public courses: Course[] = [];
+  public editCourse: Course | undefined = undefined;
+  public difficulties: Difficulty[] = [];
 
   constructor(
-    public route:ActivatedRoute,
-    public api:PenocApiService,
-    public dataCache:DataCacheService,
-    private router:Router
-  ){}
+    public route: ActivatedRoute,
+    public api: PenocApiService,
+    public dataCache: DataCacheService,
+    private router: Router
+  ) { }
 
-  ngOnInit(){
-    this.loadEventCourses(Number(this.route.snapshot.paramMap.get('oEventId')));
+  ngOnInit() {
+    let eventId = Number(this.route.snapshot.paramMap.get('oEventId'))
+    this.loadEvent(eventId);
+    this.loadEventCourses(eventId);
     this.dataCache.getDifficulties().subscribe(data => this.difficulties = data);
   }
 
-  public loadEventCourses(eventId:number){
-    this.eventId = eventId;
-    this.api.getOEventCourses(eventId).subscribe(data => {this.courses = data});
+  public loadEvent(eventId: number) {
+    this.api.getOEvent(eventId).subscribe(data => { this.oEvent = data });
   }
 
-  onEditClick(courseId:number){
-    let course:Course|undefined = this.courses.find(course => course.id === courseId);
-    if(course){
+  public loadEventCourses(eventId: number) {
+    this.api.getOEventCourses(eventId).subscribe(data => { this.courses = data });
+  }
+
+  onEditClick(courseId: number) {
+    let course: Course | undefined = this.courses.find(course => course.id === courseId);
+    if (course) {
       this.editCourse = { ...course }
     }
   }
 
-  onSaveClick(){
-    if(this.editCourse){
-      if(this.editCourse.id > 0){
-        this.api.saveCourse(this.editCourse).subscribe(data =>{
-        this.editCourse = undefined; 
-        this.loadEventCourses(this.eventId);})}
+  onSaveClick() {
+    if (this.editCourse) {
+      if (this.editCourse.id > 0) {
+        this.api.saveCourse(this.editCourse).subscribe(data => {
+          this.editCourse = undefined;
+          this.loadEventCourses(this.oEvent!.id!);
+        })
+      }
       else {
         this.api.addCourse(this.editCourse).subscribe(data => {
-        this.editCourse = undefined; 
-        this.loadEventCourses(this.eventId)})
+          this.editCourse = undefined;
+          this.loadEventCourses(this.oEvent!.id!);
+        })
       }
     }
   }
 
-  onCancelClick(){
+  onCancelClick() {
     this.editCourse = undefined;
   }
 
-  onNewClick(){
+  onNewClick() {
     this.editCourse = new Course();
-    this.editCourse.eventId = this.eventId;
+    this.editCourse.eventId = this.oEvent!.id!;
   }
 
-  onDeleteClick(courseId:number){  
+  onDeleteClick(courseId: number) {
     this.api.deleteCourse(courseId).subscribe(data => {
-      this.loadEventCourses(this.eventId);
+      this.loadEventCourses(this.oEvent!.id!);
     });
   }
 
-  onNameClick(courseId:number){
+  onNameClick(courseId: number) {
     this.router.navigate(['admin/course-results-edit', courseId]);
   }
 }
