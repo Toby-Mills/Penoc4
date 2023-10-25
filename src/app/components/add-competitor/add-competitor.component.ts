766c5e449613d3967daae42b1684904049bf777d
Toby Mills <toby_mills@trimble.com>
2023-10-25 14:33:55 +0200

    functionality for add-competitor component

--- src/app/components/add-competitor/add-competitor.component.ts
+++ src/app/components/add-competitor/add-competitor.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Output } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component, EventEmitter } from '@angular/core';
+import { Competitor } from 'src/app/models/competitor';
+import { PenocApiService } from 'src/app/services/penoc-api.service';
 
 @Component({
   selector: 'app-add-competitor',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-competitor.component.css']
 })
 export class AddCompetitorComponent {
+  public competitor: Competitor = new Competitor();
+  @Output() newCompetitor: EventEmitter<Competitor> = new EventEmitter();
+  @Output() cancel: EventEmitter<void> = new EventEmitter();
 
+  public constructor(
+    public api: PenocApiService,
+    public dialogRef: DialogRef
+  ) { }
+
+  onSaveClick() {
+    this.api.addCompetitor(this.competitor).subscribe(newCompetitor => {
+      this.newCompetitor.emit(newCompetitor);
+      this.dialogRef.close();
+    })
+  }
+
+  onCancelClick() {
+    this.cancel.emit();
+    this.dialogRef.close();
+  }
 }
+
+
