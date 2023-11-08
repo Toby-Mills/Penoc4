3f05202d351128f44b0fab2ad336e660970879f7
Toby Mills <toby_mills@trimble.com>
2023-11-08 16:12:23 +0200

    Ability to edit existing competitors

--- src/app/components/add-edit-competitor/add-edit-competitor.component.ts
+++ src/app/components/add-edit-competitor/add-edit-competitor.component.ts
@@ -1,8 +1,7 @@
-import { Output } from '@angular/core';
-import { DialogRef } from '@angular/cdk/dialog';
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
+import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
 import { Competitor } from 'src/app/models/competitor';
-import { PenocApiService } from 'src/app/services/penoc-api.service';
+import { DataCacheService } from 'src/app/services/data-cache.service';
 
 @Component({
   selector: 'app-add-edit-competitor',
@@ -11,19 +10,44 @@ import { PenocApiService } from 'src/app/services/penoc-api.service';
 })
 export class AddEditCompetitorComponent {
   public competitor: Competitor = new Competitor();
+  @Input() competitorToEditId: number | undefined;
   @Output() newCompetitor: EventEmitter<Competitor> = new EventEmitter();
+  @Output() updatedCompetitor: EventEmitter<Competitor> = new EventEmitter();
   @Output() cancel: EventEmitter<void> = new EventEmitter();
+  public addingNew: Boolean = false;
 
   public constructor(
-    public api: PenocApiService,
-    public dialogRef: DialogRef
+    @Inject(DIALOG_DATA) public data: { competitorToEditId: number | undefined },
+    public dataService: DataCacheService,
+    public dialogRef: DialogRef,
   ) { }
 
+  ngOnInit() {
+    if (this.data.competitorToEditId) {
+      this.competitorToEditId = this.data.competitorToEditId
+    }
+    if (this.competitorToEditId) {
+      this.dataService.getCompetitor(this.competitorToEditId).subscribe(competitor => {
+        let competitorClone  = {...competitor};
+        this.competitor = competitorClone;
+      })
+      this.addingNew = false;
+    } else {
+      this.addingNew = true;
+    }
+  }
+
   onSaveClick() {
-    this.api.addCompetitor(this.competitor).subscribe(newCompetitor => {
+    if(this.addingNew){
+    this.dataService.addCompetitor(this.competitor).subscribe(newCompetitor => {
       this.newCompetitor.emit(newCompetitor);
       this.dialogRef.close();
-    })
+    })} else {
+      this.dataService.updateCompetitor(this.competitor).subscribe(updatedCompetitor => {
+        this.updatedCompetitor.emit(updatedCompetitor);
+        this.dialogRef.close();
+      })
+    }
   }
 
   onCancelClick() {
